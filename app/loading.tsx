5b4470b67ffadf6321de1669cc084ed8a8772f22
General owner <ajibahtiar532@gmail.com>
2026-06-11 05:57:29 +0700

    Create loading.tsx

---
+++ app/loading.tsx
@@ -0,0 +1,15 @@
+export default function Loading() {
+  return (
+    <main
+      style={{
+        minHeight: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        color: "white",
+      }}
+    >
+      🚀 Loading PrimeSpace...
+    </main>
+  );
+}
