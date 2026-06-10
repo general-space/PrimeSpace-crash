78fea85fca260fec3aa7a6f3eb9e68b8f1fdace3
General owner <ajibahtiar532@gmail.com>
2026-06-10 23:36:18 +0700

    Update owner.ts

--- bot/owner.ts
+++ bot/owner.ts
@@ -1,5 +1,12 @@
-const OWNER_ID =
-  Number(process.env.OWNER_ID);
+const ownerId = process.env.OWNER_ID;
+
+if (!ownerId) {
+  throw new Error(
+    "OWNER_ID is missing"
+  );
+}
+
+const OWNER_ID = Number(ownerId);
 
 export function isOwner(
   telegramId: number
