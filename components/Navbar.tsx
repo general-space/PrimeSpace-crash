b9d572c54e161b83d5ab9af7c1e6709ef735204e
General owner <ajibahtiar532@gmail.com>
2026-06-10 22:07:00 +0700

    Create Navbar.tsx

---
+++ components/Navbar.tsx
@@ -0,0 +1,16 @@
+export default function Navbar() {
+  return (
+    <nav
+      style={{
+        padding: "20px",
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+      }}
+    >
+      <h2>🚀 PrimeSpace</h2>
+
+      <span>Balance: 0 Coin</span>
+    </nav>
+  );
+}
