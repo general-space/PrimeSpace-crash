fb239086e4be2a13aa9123c3130d2e8e70053201
General owner <ajibahtiar532@gmail.com>
2026-06-10 22:49:00 +0700

    Update page.tsx

--- app/game/page.tsx
+++ app/game/page.tsx
@@ -1,18 +1,27 @@
 import RocketGame from "../../components/RocketGame";
+import BetPanel from "../../components/BetPanel";
+import CosmicBackground from "../../components/CosmicBackground";
 
 export default function GamePage() {
   return (
-    <main
-      style={{
-        minHeight: "100vh",
-        background: "#050816",
-        color: "white",
-        padding: "20px",
-      }}
-    >
-      <h1>🚀 PrimeSpace Game</h1>
+    <>
+      <CosmicBackground />
 
-      <RocketGame />
-    </main>
+      <main
+        style={{
+          minHeight: "100vh",
+          color: "white",
+          padding: "20px",
+          maxWidth: "1000px",
+          margin: "0 auto",
+        }}
+      >
+        <h1>🚀 PrimeSpace Game</h1>
+
+        <RocketGame />
+
+        <BetPanel />
+      </main>
+    </>
   );
 }
