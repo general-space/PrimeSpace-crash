9cbc2a6405af853d0184690ee4580092080a9965
General owner <ajibahtiar532@gmail.com>
2026-06-10 23:26:43 +0700

    Create not-found.tsx

---
+++ app/not-found.tsx
@@ -0,0 +1,18 @@
+export default function NotFound() {
+  return (
+    <main
+      style={{
+        minHeight: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        color: "white",
+      }}
+    >
+      <h1>404</h1>
+
+      <p>Page Not Found</p>
+    </main>
+  );
+}
