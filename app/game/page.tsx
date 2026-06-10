35a9c102a3896dcb7bc3c1b5445a563dde5d4126
General owner <ajibahtiar532@gmail.com>
2026-06-10 22:12:25 +0700

    Create page.tsx

---
+++ app/game/page.tsx
@@ -0,0 +1,36 @@
+export default function GamePage() {
+  return (
+    <main
+      style={{
+        minHeight: "100vh",
+        background: "#050816",
+        color: "white",
+        padding: "20px",
+      }}
+    >
+      <h1>🚀 PrimeSpace Game</h1>
+
+      <div
+        style={{
+          marginTop: "40px",
+          textAlign: "center",
+        }}
+      >
+        <h2>🚀</h2>
+
+        <h1>x1.00</h1>
+
+        <button
+          style={{
+            marginTop: "20px",
+            padding: "12px 24px",
+            borderRadius: "12px",
+            border: "none",
+          }}
+        >
+          START GAME
+        </button>
+      </div>
+    </main>
+  );
+}
