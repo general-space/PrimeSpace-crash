9ad53b5d6d823dc737636e6fd7ba42ae951ebe15
General owner <ajibahtiar532@gmail.com>
2026-06-10 23:18:21 +0700

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Navbar() {
   return (
     <nav
@@ -6,11 +8,50 @@ export default function Navbar() {
         display: "flex",
         justifyContent: "space-between",
         alignItems: "center",
+        gap: "20px",
       }}
     >
-      <h2>🚀 PrimeSpace</h2>
+      <Link
+        href="/"
+        style={{
+          textDecoration: "none",
+          color: "white",
+          fontWeight: "bold",
+          fontSize: "20px",
+        }}
+      >
+        🚀 PrimeSpace
+      </Link>
 
-      <span>Balance: 0 Coin</span>
+      <div
+        style={{
+          display: "flex",
+          gap: "15px",
+          alignItems: "center",
+        }}
+      >
+        <Link href="/" style={{ color: "white" }}>
+          Home
+        </Link>
+
+        <Link href="/game" style={{ color: "white" }}>
+          Game
+        </Link>
+
+        <Link href="/profile" style={{ color: "white" }}>
+          Profile
+        </Link>
+
+        <Link href="/leaderboard" style={{ color: "white" }}>
+          Leaderboard
+        </Link>
+
+        <Link href="/history" style={{ color: "white" }}>
+          History
+        </Link>
+
+        <span>💰 0 Coin</span>
+      </div>
     </nav>
   );
 }
