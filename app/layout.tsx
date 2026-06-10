59578be30871b198950ed07ffc035f8bda60b882
General owner <ajibahtiar532@gmail.com>
2026-06-10 22:05:48 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,18 @@
+import "./globals.css";
+
 export const metadata = {
   title: "PrimeSpace Crash",
-  description: "Cosmic Space Crash Experience"
-}
+  description: "Cosmic Space Crash Experience",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
-      <body>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
-  )
+  );
 }
