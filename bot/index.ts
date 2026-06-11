160f3aa0f9580b05e70ae89b641e90a069430e98
General owner <ajibahtiar532@gmail.com>
2026-06-11 20:38:59 +0700

    Update index.ts

--- bot/index.ts
+++ bot/index.ts
@@ -1,12 +1,16 @@
 import { Telegraf } from "telegraf";
 
-const botToken = process.env.BOT_TOKEN;
+const botToken =
+  process.env.BOT_TOKEN;
 
 if (!botToken) {
-  throw new Error("BOT_TOKEN is missing");
+  throw new Error(
+    "BOT_TOKEN is missing"
+  );
 }
 
-export const bot = new Telegraf(botToken);
+export const bot =
+  new Telegraf(botToken);
 
 import "./start";
 import "./commands";
@@ -14,17 +18,3 @@ import "./addcoin";
 import "./removecoin";
 import "./stats";
 import "./ban";
-
-bot.launch();
-
-console.log(
-  "🚀 PrimeSpace Crash Bot Online"
-);
-
-process.once("SIGINT", () =>
-  bot.stop("SIGINT")
-);
-
-process.once("SIGTERM", () =>
-  bot.stop("SIGTERM")
-);
