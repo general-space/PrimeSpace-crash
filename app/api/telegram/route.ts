e9a6515be8fda6f09e2b436eb4fea6f2049f4f08
General owner <ajibahtiar532@gmail.com>
2026-06-11 20:29:38 +0700

    Create route.ts

---
+++ app/api/telegram/route.ts
@@ -0,0 +1,14 @@
+import { bot } from "@/bot";
+
+export async function POST(
+  request: Request
+) {
+  const update =
+    await request.json();
+
+  await bot.handleUpdate(update);
+
+  return Response.json({
+    ok: true,
+  });
+}
